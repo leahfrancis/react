fca0b0f76a91962489c820ea027285bbd128c595
leah <leahcalicut@gmail.com>
2024-05-08 12:37:45 +0530

    todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [toDos, setToDos] = useState([])
+  const [toDo, setToDo] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input 
+          type="text" 
+          value={toDo}
+          onChange={(e) => setToDo(e.target.value)} 
+          placeholder="🖊️ Add item..." 
+        />
+        <i 
+          onClick={() => setToDos([...toDos, { id: Date.now(), text: toDo, status: false }])} 
+          className="fas fa-plus"
+        ></i>
+      </div>
+      <div className="todos">
+        <h2>Completed Tasks</h2>
+        {toDos.map((obj) => {
+          if (obj.status) {
+            return (
+              <h2 key={obj.id}>{obj.text}</h2>
+            )
+          }
+          return null
+        })}
+        <h2>Tasks</h2>
+        {toDos.map((obj) => (
+          <div className="todo" key={obj.id}>
+            <div className="left">
+              <input 
+                onChange={(e) => {
+                  const updatedToDos = toDos.map(todo => 
+                    todo.id === obj.id ? { ...todo, status: e.target.checked } : todo
+                  );
+                  setToDos(updatedToDos);
+                }}
+                value={obj.status} 
+                type="checkbox" 
+                name="" 
+                id="" 
+              />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
